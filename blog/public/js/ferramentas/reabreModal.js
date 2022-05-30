9cf36824906e973003e8da441e93ddc85ccf61c0
LuisJose66 <ljrri66@gmail.com>
2022-05-30 17:04:10 -0300

    jose luis 30/05/22

--- blog/public/js/ferramentas/reabreModal.js
+++ blog/public/js/ferramentas/reabreModal.js
@@ -1,22 +1,31 @@
-$('.modal-botao, .pag-item').click(function() {
+$('.modal-botao, .pag-item, .page-link').click(function() {
     localStorage.setItem("modal", "enabled");
 });
 
 function verficarModal(idModal) {
-    var valueModal = localStorage.getItem('modal');
+    var valueModal = localStorage.getItem("modal");
     if (valueModal === "enabled") {
       $(document).ready(function() {
         $(idModal).modal("show");
+        localStorage.setItem("modal", "disabled");
       });
-      localStorage.setItem("modal", "disabled");
+      
     }
 }
 
- var modalTrabalhador = verficarModal('#modalTrabalhador');
- var modalTomador = verficarModal('#modalTomador');
- var modalTabPreco = verficarModal('#modalTabPreco');
- var modalComissionado = verficarModal('#modalComissionado');
- var modalCadAcesso = verficarModal('#modalCadAcesso');
- var modalDesconto = verficarModal('#modalDesconto');
+var modalTrabalhador = verficarModal('#modalTrabalhador');
+var modalTomador = verficarModal('#modalTomador');
+var modalTabPreco = verficarModal('#modalTabPreco');
+var modalComissionado = verficarModal('#modalComissionado');
+var modalCadAcesso = verficarModal('#modalCadAcesso');
+var modalDesconto = verficarModal('#modalDesconto');
+var modalCartaoPonto = verficarModal('#modalCartaoPonto');
+var modalListaDiurno = verficarModal('#listaDiurno');
+var modalListaNoturno = verficarModal('#listaNoturno');
+var modalBoletimTabela = verficarModal('#modalBoletimTabela');
+var modalBoletimTabelaLancamento = verficarModal('#modalLancamentoPreco');
+
+
+
  
 
